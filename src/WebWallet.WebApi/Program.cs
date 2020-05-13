a3a9513de8ae0bf09b343cbf218f46d368ee0813
Arius Bronte <ariusbronte@gmail.com>
2020-05-13 19:28:42 +0000

    Cleanup code: removed, updated initial code

--- src/WebWallet.WebApi/Program.cs
+++ src/WebWallet.WebApi/Program.cs
@@ -1,22 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebWallet.WebApi
 {
+    /// <summary>
+    ///     Entry point class of programme.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        ///     Entry method of programme.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Microsoft.Extensions.Hosting.HostBuilder"/> class with pre-configured defaults.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>The <see cref="Microsoft.Extensions.Hosting.IHostBuilder"/> so that additional calls can be chained.</returns>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
--- src/WebWallet.WebApi/Startup.cs
+++ src/WebWallet.WebApi/Startup.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,23 +8,48 @@ using Microsoft.Extensions.Logging;
 
 namespace WebWallet.WebApi
 {
+    /// <summary>
+    ///      Configures services and the app's request pipeline.
+    /// </summary>
     public class Startup
     {
+        /// <inheritdoc cref="Microsoft.Extensions.Configuration.IConfiguration"/>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="WebWallet.WebApi.Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">
+        ///     Represents a set of key/value application configuration properties.
+        /// </param>
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
 
-        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">Defines a contract for a collection of service descriptors.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">
+        ///     Provides the mechanisms to configure an application's request pipeline.
+        /// </param>
+        /// <param name="env">
+        ///     Provides information about the web hosting environment an application is running in.
+        /// </param>
+        /// <param name="logger">
+        ///     Represents a type used to perform logging.
+        /// </param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -37,11 +57,8 @@ namespace WebWallet.WebApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
