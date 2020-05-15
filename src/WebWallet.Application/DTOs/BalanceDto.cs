e9b021b5396985352c829b99b36e11cd8278fe71
Arius Bronte <ariusbronte@gmail.com>
2020-05-15 00:27:56 +0000

    Create BalanceDto.cs

---
+++ src/WebWallet.Application/DTOs/BalanceDto.cs
@@ -0,0 +1,20 @@
+﻿using WebWallet.Domain.Enums;
+
+namespace WebWallet.Application.DTOs
+{
+    /// <summary>
+    ///     The user's balance info.
+    /// </summary>
+    public class BalanceDto
+    {
+        /// <summary>
+        ///     The user's balance.
+        /// </summary>
+        public decimal Balance { get; set; }
+        
+        /// <summary>
+        ///     The balance currency.
+        /// </summary>
+        public Currency Currency { get; set; }
+    }
+}
