b7fe223f6cc5ed8093d79869b529c6abf8bab27e
Arius Bronte <ariusbronte@gmail.com>
2020-05-15 00:33:07 +0000

    Created WithdrawCommand, WithdrawCommandHandler

---
+++ src/WebWallet.Application/Wallet/Commands/Withdraw/WithdrawCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using WebWallet.Domain.Enums;
+
+namespace WebWallet.Application.Wallet.Commands.Withdraw
+{
+    public class WithdrawCommand : IRequest<Unit>
+    {
+        /// <summary>
+        ///     The primary user key.
+        /// </summary>
+        public long UserId { get; set; }
+        
+        /// <summary>
+        ///     The amount you need to cash out.
+        /// </summary>
+        public decimal Withdraw { get; set; }
+        
+        /// <summary>
+        ///     The currency that the amount will be cashed from.
+        /// </summary>
+        public Currency Currency { get; set; } 
+    }
+}
---
+++ src/WebWallet.Application/Wallet/Commands/Withdraw/WithdrawCommandHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WebWallet.Application.Exceptions;
+using WebWallet.DataAccess;
+using WebWallet.Domain.Entites;
+using WebWallet.Domain.Enums;
+
+namespace WebWallet.Application.Wallet.Commands.Withdraw
+{
+    public class WithdrawCommandHandler : IRequestHandler<WithdrawCommand, Unit>
+    {
+        private readonly WebWalletDbContext _dbContext;
+
+        public WithdrawCommandHandler(WebWalletDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<Unit> Handle(WithdrawCommand request, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var userId = request.UserId;
+            var withdraw = request.Withdraw;
+            var currency = request.Currency;
+
+            var userExists = await UserAnyAsync(userId, cancellationToken);
+            if (!userExists)
+            {
+                throw new UserNotFoundException(nameof(UserEntity), userId);
+            }
+
+            var wallet = await GetWalletAsync(userId, currency, cancellationToken);
+            if (wallet == null)
+            {
+                throw new WalletNotFoundException(nameof(WalletEntity), $"{nameof(currency)}: {currency}");
+            }
+
+            if (wallet.Balance < withdraw)
+            {
+                throw new BalanceNotEnoughException();
+            }
+
+            wallet.SubtractBalance(withdraw);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+
+        private async Task<bool> UserAnyAsync(long userId, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _dbContext.Users
+                .AsNoTracking()
+                .AnyAsync(x => x.Id == userId, cancellationToken);
+        }
+
+        private async Task<WalletEntity> GetWalletAsync(
+            long userId,
+            Currency currency,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _dbContext.Wallets
+                .FirstOrDefaultAsync(x =>
+                    x.UserEntityId == userId && x.Currency == currency, cancellationToken);
+        }
+    }
+}
