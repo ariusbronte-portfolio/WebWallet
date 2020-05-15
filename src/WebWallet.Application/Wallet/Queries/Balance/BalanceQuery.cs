77fcad3a114254d8e77d08a1bccac4ca92db8bb8
Arius Bronte <ariusbronte@gmail.com>
2020-05-15 00:32:26 +0000

    Created BalanceQuery, BalanceQueryHandler

---
+++ src/WebWallet.Application/Wallet/Queries/Balance/BalanceQuery.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using WebWallet.Application.DTOs;
+
+namespace WebWallet.Application.Wallet.Queries.Balance
+{
+    public class BalanceQuery : IRequest<IEnumerable<BalanceDto>>
+    {
+        public BalanceQuery(long userId)
+        {
+            UserId = userId;
+        }
+        
+        /// <summary>
+        ///     The primary user key.
+        /// </summary>
+        public long UserId { get; }
+    }
+}
---
+++ src/WebWallet.Application/Wallet/Queries/Balance/BalanceQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WebWallet.Application.DTOs;
+using WebWallet.DataAccess;
+
+namespace WebWallet.Application.Wallet.Queries.Balance
+{
+    public class BalanceQueryHandler : IRequestHandler<BalanceQuery, IEnumerable<BalanceDto>>
+    {
+        private readonly WebWalletDbContext _dbContext;
+
+        public BalanceQueryHandler(WebWalletDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IEnumerable<BalanceDto>> Handle(BalanceQuery request, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            var userId = request.UserId;
+
+            return await _dbContext.Wallets
+                .AsNoTracking()
+                .Where(x => x.UserEntityId == userId)
+                .Select(x => new BalanceDto
+                {
+                    Balance = x.Balance,
+                    Currency = x.Currency
+                }).ToArrayAsync(cancellationToken);
+        }
+    }
+}
